9801d1e4be30270b7d626e6fe3eaef340cf1e049
Kim-777 <iloo3012@naver.com>
2022-03-11 16:15:54 +0900

    init

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  textColor: "black",
+  bgColor: "whitesmoke",
+  accentColor: "#9c88ff",
+};
+
+export const darkTheme: DefaultTheme = {
+  textColor: "f5f6fa",
+  bgColor: "#524949",
+  accentColor: "#9c88ff",
+};
