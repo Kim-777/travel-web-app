3e37d6064c6a50b3929a91678cd70d8f0e7b51cb
Kim-777 <iloo3012@naver.com>
2022-03-12 15:28:58 +0900

    practice Recoil

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,105 +1,36 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-
-interface IFormData {
-  email: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
-}
+import { useRecoilState, useRecoilValue } from "recoil";
+import { Categories, categoryState, toDoSelector } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
 export default function ToDoList() {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IFormData>();
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  // console.log("todos :: ", todos);
 
-  const onValid = (data: IFormData) => {
-    if (data.password !== data.password1) {
-      return setError(
-        "password1",
-        {
-          message: "패스워드가 다릅니다.",
-        },
-        {
-          shouldFocus: true,
-        }
-      );
-    }
-    setError("extraError", { message: "Server 에러..." });
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value as Categories);
   };
-
-  console.log("formState.errors :: ", errors);
-
-  // console.log(watch());
-  // const [toDo, setToDo] = React.useState("");
-
-  // const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-  //   setToDo(event.currentTarget.value);
-  // };
-
-  // const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  // };
 
   return (
     <div>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: "20px 30px",
-        }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        <input
-          {...register("email", {
-            required: "emial is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "naver 이메일만 허용됩니다.",
-            },
-          })}
-          placeholder="email"
-        />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("username", {
-            required: "이름을 입력해주세요",
-            minLength: {
-              value: 2,
-              message: "이름은 2글자 이상 입력해야 합니다.",
-            },
-          })}
-          placeholder="username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", {
-            required: "패스워드를 입력해주세요",
-            minLength: {
-              value: 5,
-              message: "패스워드를 5글자 이상 입력해주세요",
-            },
-          })}
-          placeholder="password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", {
-            required: "5글자 이상 입력해주세요",
-            minLength: 5,
-          })}
-          placeholder="password1"
-        />
-        <span>{errors?.password1?.message}</span>
-        <button>추가</button>
-        <span>{errors?.extraError?.message}</span>
+      <h1>To Dos</h1>
+      <hr />
+      <form>
+        <select value={category} onInput={onInput}>
+          <option value={Categories.TO_DO}>To Do</option>
+          <option value={Categories.DOING}>Doing</option>
+          <option value={Categories.DONE}>Done</option>
+        </select>
       </form>
+      <CreateToDo />
+      <h2>{category}</h2>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo key={`${toDo.id}`} {...toDo} />
+        ))}
+      </ul>
     </div>
   );
 }
